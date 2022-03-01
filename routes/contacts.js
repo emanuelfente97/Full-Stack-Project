222e04406951891df19ae023577752562fb0b739
emanuelfente97 <emanuelfente97@gmail.com>
2022-03-01 11:48:12 +0200

    first push

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const nodemailer = require('nodemailer');
 const router = express.Router();
-require('dotenv').config();
+// require('dotenv').config();
 
 app.get('/',(req, res) => res.send({ msg: 'send contact using POST' }));
 
